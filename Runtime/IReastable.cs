f28095b551469ea3f96ac1ef74a6eae887131d80
Hyago Oliveira <hyagogow@gmail.com>
2023-01-10 13:55:57 -0300

    Add IReastable interface

---
+++ Runtime/IReastable.cs
@@ -0,0 +1,9 @@
+namespace ActionCode.PauseSystem
+{
+    /// <summary>
+    /// Interface used on objects able to Pause and Resume.
+    /// </summary>
+    public interface IReastable : IPauseable, IResumable 
+    {
+    }
+}
