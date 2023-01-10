3a4beec898ac621a0ba1270a177a0e1526ad0e25
Hyago Oliveira <hyagogow@gmail.com>
2023-01-10 14:16:15 -0300

    Remove IRestable interface

--- Runtime/Interfaces/IRestable.cs
+++
@@ -1,9 +0,0 @@
-namespace ActionCode.PauseSystem
-{
-    /// <summary>
-    /// Interface used on objects able to Pause and Resume.
-    /// </summary>
-    public interface IRestable : IPauseable, IResumable 
-    {
-    }
-}
