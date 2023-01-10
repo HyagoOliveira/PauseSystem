228cf90a8dc22d89476cb42c04970ce8eb230e1c
Hyago Oliveira <hyagogow@gmail.com>
2023-01-10 14:04:05 -0300

    Fix typo

--- Runtime/Interfaces/IRestable.cs
+++ Runtime/Interfaces/IRestable.cs
@@ -3,7 +3,7 @@ namespace ActionCode.PauseSystem
     /// <summary>
     /// Interface used on objects able to Pause and Resume.
     /// </summary>
-    public interface IReastable : IPauseable, IResumable 
+    public interface IRestable : IPauseable, IResumable 
     {
     }
 }
