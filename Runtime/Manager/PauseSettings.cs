b325e4a89525073cfad0c43d3741bec32f5a95f2
Hyago Oliveira <hyagogow@gmail.com>
2023-01-10 14:21:05 -0300

    Add PauseSettings

---
+++ Runtime/Manager/PauseSettings.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace ActionCode.PauseSystem
+{
+    /// <summary>
+    /// Settings class for 
+    /// </summary>
+    [CreateAssetMenu(fileName = "PauseSettings", menuName = "ActionCode/Pause Settings", order = 110)]
+    public sealed class PauseSettings : ScriptableObject, IPauseable
+    {
+        private readonly List<IPauseable> pauseables = new();
+
+        internal void Initialize ()
+        {
+            pauseables.Clear();
+        }
+        
+        public void Pause()
+        {
+            foreach (var pauseable in pauseables)
+            {
+                pauseable.Pause();
+            }
+        }
+
+        public void Resume()
+        {
+            foreach (var pauseable in pauseables)
+            {
+                pauseable.Resume();
+            }
+        }
+
+        public void Register(IPauseable pauseable)
+        {
+            var canAdd = !pauseables.Contains(pauseable);
+            if (canAdd) pauseables.Add(pauseable);
+        }
+
+        public void Unregister(IPauseable pauseable)
+        {
+            var canRemove = pauseables.Contains(pauseable);
+            if (canRemove) pauseables.Remove(pauseable);
+        }
+    }
+}
