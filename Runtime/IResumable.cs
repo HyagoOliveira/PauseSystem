7bd87be86a0395275b9cbd876623cf9f266e6821
Hyago Oliveira <hyagogow@gmail.com>
2023-01-10 13:49:42 -0300

    Add IResumable interface

---
+++ Runtime/IResumable.cs
@@ -0,0 +1,13 @@
+namespace ActionCode.PauseSystem
+{
+    /// <summary>
+    /// Interface used on objects able to Resume.
+    /// </summary>
+    public interface IResumable 
+    {
+        /// <summary>
+        /// Resumes the instance.
+        /// </summary>
+        void Resume();
+    }
+}
