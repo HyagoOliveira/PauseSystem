5bc88c20d62ea57622b7d389f36c03f6a6b8db21
Hyago Oliveira <hyagogow@gmail.com>
2023-01-11 10:30:05 -0300

    Add PauseManager

---
+++ Runtime/Manager/PauseManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace ActionCode.PauseSystem
+{
+    /// <summary>
+    /// Manager for the Pause System. It initializes the <see cref="PauseSettings"/>.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public sealed class PauseManager : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The Pause Settings asset reference.")]
+        private PauseSettings settings;
+
+        private void Start () => settings.Initialize();
+    }
+}
