b5d01a948b18ddf4449203654a607e45c03e7f41
Hyago Oliveira <hyagogow@gmail.com>
2023-01-11 10:40:16 -0300

    Implement stopTimeScale field

--- Runtime/Manager/PauseSettings.cs
+++ Runtime/Manager/PauseSettings.cs
@@ -9,12 +9,17 @@ namespace ActionCode.PauseSystem
     [CreateAssetMenu(fileName = "PauseSettings", menuName = "ActionCode/Pause Settings", order = 110)]
     public sealed class PauseSettings : ScriptableObject, IPauseable
     {
+        [SerializeField, Tooltip("Whether to stop the TimeScale when pause.")]
+        private bool stopTimeScale = true;
+        
         private readonly List<IPauseable> pauseables = new();
 
         internal void Initialize () => pauseables.Clear();
         
         public void Pause()
         {
+            if (stopTimeScale) Time.timeScale = 0f;
+
             foreach (var pauseable in pauseables)
             {
                 pauseable.Pause();
@@ -23,6 +28,8 @@ namespace ActionCode.PauseSystem
 
         public void Resume()
         {
+            if (stopTimeScale) Time.timeScale = 1f;
+
             foreach (var pauseable in pauseables)
             {
                 pauseable.Resume();
