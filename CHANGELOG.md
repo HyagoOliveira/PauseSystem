6d3a06bf08149d6bda02b3d6b71f8e45ec047c78
Hyago Oliveira <hyagogow@gmail.com>
2023-01-13 10:56:21 -0300

    Add PauseBehaviours

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [Unreleased]
 ### Added
 - Awaitable Coroutines package dependency
+- PauseBehaviours
 
 ### Fixed
 - Time.deltaTime is not 0F when OnResume is invoked
---
+++ Runtime/Components/PauseBehaviours.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace ActionCode.PauseSystem
+{
+    /// <summary>
+    /// Pauses and resumes all Behaviour components.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public sealed class PauseBehaviours : MonoBehaviour 
+    {
+        [SerializeField, Tooltip("The Pause Settings asset.")]
+        private PauseSettings settings;
+        [SerializeField, Tooltip("All local Behaviour components. They will be paused/resumed.")]
+        private Behaviour[] behaviours;
+        
+        private void Reset ()
+        {
+            behaviours = GetComponentsInChildren<Behaviour>(includeInactive: true);
+        }
+        
+        private void OnEnable ()
+        {
+            settings.OnPaused += HandlePaused;
+            settings.OnResumed += HandleResumed;
+        }
+        
+        private void OnDisable ()
+        {
+            settings.OnPaused -= HandlePaused;
+            settings.OnResumed -= HandleResumed;
+        }
+
+        private void HandlePaused () => SetBehavioursEnabled(false);
+	    
+        private void HandleResumed () => SetBehavioursEnabled(true);
+
+        private void SetBehavioursEnabled (bool enabled)
+        {
+            foreach (var behaviour in behaviours)
+            {
+                behaviour.enabled = enabled;
+            }
+        }
+    }
+}
