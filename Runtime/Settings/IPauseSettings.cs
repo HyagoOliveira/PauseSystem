1c1d51d2aaf565791126481e7eb0b021d2da0e15
Hyago Oliveira <hyagogow@gmail.com>
2024-11-18 18:13:41 -0300

    Remove IPauseSettings interface

--- Runtime/Settings/IPauseSettings.cs
+++ Runtime/Settings/IPauseSettings.cs
@@ -5,41 +5,27 @@ namespace ActionCode.PauseSystem
     /// <summary>
     /// Settings interface for the Pause System.
     /// </summary>
-    public interface IPauseSettings 
+    public interface IPauseSettings
     {
-        /// <summary>
-        /// Action fired when the game is paused.
-        /// </summary>
+
         event Action OnPaused;
-        
-        /// <summary>
-        /// Action fired when the game is resumed.
-        /// </summary>
+
+
         event Action OnResumed;
-        
-        /// <summary>
-        /// Whether is paused.
-        /// </summary>
+
+
         bool IsPaused { get; }
-        
-        /// <summary>
-        /// Whether to stop the TimeScale when paused.
-        /// </summary>
+
+
         bool StopTimeScale { get; }
 
-        /// <summary>
-        /// Toggles between <see cref="Pause"/> and <see cref="Resume"/>.
-        /// </summary>
-        void Toggle ();
 
-        /// <summary>
-        /// Pauses the game.
-        /// </summary>
+        void Toggle();
+
+
         void Pause();
-        
-        /// <summary>
-        /// Resumes the game.
-        /// </summary>
+
+
         void Resume();
     }
 }
--- Runtime/Settings/PauseSettings.cs
+++ Runtime/Settings/PauseSettings.cs
@@ -10,23 +10,39 @@ namespace ActionCode.PauseSystem
     /// Use it to subscribe to <see cref="OnPaused"/> and <see cref="OnResumed"/> events.
     /// </summary>
     [CreateAssetMenu(fileName = "PauseSettings", menuName = "ActionCode/Pause Manager/Settings", order = 110)]
-    public sealed class PauseSettings : ScriptableObject, IPauseSettings
+    public sealed class PauseSettings : ScriptableObject
     {
         [field: SerializeField, Tooltip("Whether to stop the TimeScale when paused.")]
         public bool StopTimeScale { get; private set; } = true;
 
+        /// <summary>
+        /// Action fired when the game is paused.
+        /// </summary>
         public event Action OnPaused;
+
+        /// <summary>
+        /// Action fired when the game is resumed.
+        /// </summary>
         public event Action OnResumed;
-        
+
+        /// <summary>
+        /// Whether is paused.
+        /// </summary>
         [field: NonSerialized]
         public bool IsPaused { get; private set; }
-        
-        public void Toggle ()
+
+        /// <summary>
+        /// Toggles between <see cref="Pause"/> and <see cref="Resume"/>.
+        /// </summary>
+        public void Toggle()
         {
             if (IsPaused) Resume();
             else Pause();
         }
-        
+
+        /// <summary>
+        /// Pauses the game.
+        /// </summary>
         public void Pause()
         {
             OnPaused?.Invoke();
@@ -35,6 +51,9 @@ namespace ActionCode.PauseSystem
             if (StopTimeScale) Time.timeScale = 0f;
         }
 
+        /// <summary>
+        /// Resumes the game.
+        /// </summary>
         public void Resume()
         {
             IsPaused = false;
@@ -45,11 +64,11 @@ namespace ActionCode.PauseSystem
                 _ = AwaitableCoroutine.Run(WaitAndInvokeResume());
                 return;
             }
-            
+
             OnResumed?.Invoke();
         }
 
-        private IEnumerator WaitAndInvokeResume ()
+        private IEnumerator WaitAndInvokeResume()
         {
             // Time.deltaTime would be 0F if not waiting for the Fixed Update.
             yield return new WaitForFixedUpdate();
